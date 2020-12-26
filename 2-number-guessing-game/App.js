8342bb9c30a14ab32fd0837994b9f8ffd0547346
Anton Javits <anton.javits@gmail.com>
2020-12-26 21:14:12 +0200

    Initial

--- 2-number-guessing-game/App.js
+++ 2-number-guessing-game/App.js
@@ -1,14 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native'
 
 export default function App() {
+
+  const defaultOutput = 'Guess a number between 1-100'
+  const newRandomNumber = Math.floor(Math .random() * 100) + 1
+  const [guessedNumber, setGuessedNumber] = useState(0)
+  const [guessCount, setGuessCount] = useState(0)
+  const [outputText, setOutputText] = useState(defaultOutput)
+  const [randomNumber, setRandomNumer] = useState(newRandomNumber)
+
+  console.warn(`Random number is ${randomNumber}, count: ${guessCount}`)
+
+  const handleInputChangeNumber = (guessedNumber) => {
+    setGuessedNumber(parseInt(guessedNumber.replace(/[^0-9]/g, '')) )
+  }
+
+  const guessMade = () => {
+    if (guessedNumber > randomNumber) {
+      setOutputText(`Your guess ${guessedNumber} is too high`)
+      let updatedCount = guessCount + 1
+      setGuessCount(updatedCount)
+    }
+    if (guessedNumber < randomNumber) {
+      setOutputText(`Your guess ${guessedNumber} is too low`)
+      let updatedCount = guessCount + 1
+      setGuessCount(updatedCount)
+    }
+    if (guessedNumber == randomNumber) {
+      Alert.alert(`You guessed the number in ${guessCount} guesses`)
+      setGuessCount(0)
+      setGuessedNumber('')
+      setRandomNumer(newRandomNumber)
+      setOutputText(defaultOutput)
+    } 
+  }
+
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <StatusBar style={styles.statusBar} barStyle={'dark-content'} />
+
+      <View style={styles.titleSection}>
+        <Text style={styles.titleHeading}>Number Guessing Game</Text>
+      </View>
+      
+      <View style={styles.bodySection}>
+        
+        <Text style={styles.outputText}>{outputText}</Text>
+        <TextInput
+          style={styles.textInputBasic}
+          onChangeText={guessedNumber => handleInputChangeNumber(guessedNumber)}
+          keyboardType='numeric'
+          value={guessedNumber}
+        />
+
+        <View style={styles.buttonGroup}>
+          <Button onPress={guessMade} title="Make Guess"/>
+        </View>
+        
+      </View>
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +75,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  StatusBar: {
+    backgroundColor: '#f2f2f2'
+  },
+  titleSection: {
+    flex: 0.7,
+    backgroundColor: '#f2f2f2',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: '#A8A8A8',
+    borderBottomWidth: 1
+  },
+  bodySection: {
+    flex: 5.3,
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonGroup: {
+    width: 200,
+    padding: 10,
+    flexDirection: 'row', 
+    justifyContent: 'space-around',
+    marginBottom: 60
+  },
+  titleHeading: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#707070',
+    textTransform: 'uppercase',
+    marginTop: 10
+  },
+  outputText: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+  textInputBasic: {
+    width: 100,
+    borderColor: '#A8A8A8',
+    borderWidth: 1,
+    padding: 5,
+    marginBottom: 10
+  }
+})
